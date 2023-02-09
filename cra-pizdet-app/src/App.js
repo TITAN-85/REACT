90e55d59a03c1ab53592981ea70049885318e09e
TITAN-85 <102234554+TITAN-85@users.noreply.github.com>
2023-02-09 13:19:04 -0500

    demo

--- cra-pizdet-app/src/App.js
+++ cra-pizdet-app/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './components/Header';
+import Image from './components/Image';
+import logo from "./img/logo512.png"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+    placeholder = 'placeholder for input';
+    render() {
+        return (
+            <div className='name'>
+                <Header title="Header string" />
+                <h1>Hello World</h1>
+                <input placeholder={this.placeholder} onClick={this.inputClick} onMouseEnter={this.mouseOver} >
+
+                </input>
+                <p>
+                    Hello
+                </p>
+                <div>
+                    <Image image={logo} />
+                </div>
+            </div>
+        )
+    }
+    inputClick() { console.log('click') }
+    mouseOver() { console.log('Mouse Over') }
 }
-
-export default App;
